49166b14690e1756b2fb8420f0e544f41e551616
kyleyiyang <kyleyiyang@gmail.com>
2020-11-08 14:19:26 -0500

    Update moveZeroes.java

--- moveZeroes.java
+++ moveZeroes.java
@@ -1,21 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        
         int j=0;
-        for (int i=0;i<nums.length;i++) {
-            if (nums[i]==0) {
+        for (int i=0; i<nums.length; i++) {
+            if (j==nums.length-1 && nums[j]==0) break;
+            while (j<nums.length && nums[j]==0) {
                 j++;
             }
-        }
-        if (j>0) {
-            Arrays.sort(nums);
-            for (int k=0;k<nums.length;k++) {
-                if (k>=nums.length-j) {
-                    nums[k]=0;
-                } else {
-                    nums[k]=nums[k+j];
-                }
+            if (j>i && j<nums.length) {
+                nums[i]=nums[j];
+                nums[j]=0;
             }
+            j++;
         }
     }
 }
